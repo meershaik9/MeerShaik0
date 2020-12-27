d4850433c7b7ffc2638bc372b31f2c04bd8811fd
meershaik9 <76646227+meershaik9@users.noreply.github.com>
2020-12-27 15:40:34 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•MEER - SHAIK -OFFICAL------•◈•
+|__\~~) •||•MEERO - BALUCH---------------•◈•
+|__(-----\  •◈•------GANGSTER-SQUAD--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  MEER
+\033[1;96mInstagram MEERU BALUCH
+\033[1;96mFacebook  MEER SHAIK
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/MeerShaik0/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  MEER SHAIK
+\033[1;96mInstagram MEERU BALUCH
+\033[1;96mFacebook  MEER SHAIK
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/MeerShaik0/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Meeru"
+CorrectPassword = "Baluch"
 
 loop = 'true'
 while (loop == 'true'):
